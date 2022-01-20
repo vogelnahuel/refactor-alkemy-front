edcc576c2ba35f710f49c78ce5120854f4d63353
vogel <vogelnahuel@gmail.com>
2022-01-20 16:24:29 -0300

    terminando refactor para subir

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,19 +5,19 @@ import {ConnectedRouter} from 'connected-react-router';
 import HomeContainer from "../pages/login/home/HomeContainer";
 import DashboardContainer from "../pages/dashboard/DashboardContainer";
 import OperationsContainer from "../pages/operations/OperationsContainer";
-// import DetailOperationsContainer from "../pages/operations/detailOperation/DetailOperationsContainer";
-// import CreateOperations from "../pages/operations/createOperation/CreateOperationContainer";
+import DetailOperationsContainer from "../pages/operations/detailOperation/DetailOperationsContainer";
+import CreateOperations from "../pages/operations/createOperation/CreateOperationContainer";
 import store, {history} from '../store'
 export const App = () => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <Switch>
-            {/* <Route path="/createOperations" component={CreateOperations} />
+            <Route path="/createOperations" component={CreateOperations} />
             <Route
               path="/operations/:id"
               component={DetailOperationsContainer}
-            /> */}
+            />
             <Route path="/operations" component={OperationsContainer} />
             <Route path="/dashboard" component={DashboardContainer} />
             <Route exact path="/" component={HomeContainer} />
